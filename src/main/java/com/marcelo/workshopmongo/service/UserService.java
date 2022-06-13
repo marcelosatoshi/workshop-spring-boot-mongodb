18e11e8205b247ec33e454bd6e57d07131d30e89
Marcelo Gomes <marcelogomestsigti@gmail.com>
2022-06-13 02:23:51 -0300

    conectando ao mongodb com repository

---
+++ src/main/java/com/marcelo/workshopmongo/service/UserService.java
@@ -0,0 +1,21 @@
+package com.marcelo.workshopmongo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcelo.workshopmongo.domain.User;
+import com.marcelo.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+
+}
